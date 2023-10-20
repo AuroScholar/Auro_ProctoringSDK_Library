d8dac800b6597e8c6b5607baf44c18fa9494106b
azzadpandit1 <azzad.pandit@aurosociety.org>
2023-10-20 11:34:31 +0530

    fix error

--- MyToolBox/build.gradle.kts
+++ MyToolBox/build.gradle.kts
@@ -117,7 +117,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.azzadpandit1"
                 artifactId = "ProctoringSDK"
-                version = "0.0.10"
+                version = "0.0.11"
             }
         }
     }
