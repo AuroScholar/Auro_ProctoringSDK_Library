021325841ac79c3d21ffaa4817e2df0f6e4bb4f6
AuroScholar <auroscholar@gmail.com>
2023-10-27 12:42:11 +0530

    27-10-2023 commit by manju WITH Own Branch with 18 version

--- MyToolBox/build.gradle.kts
+++ MyToolBox/build.gradle.kts
@@ -117,7 +117,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.azzadpandit1"
                 artifactId = "ProctoringSDK"
-                version = "0.0.14"
+                version = "0.0.18"
             }
         }
     }
