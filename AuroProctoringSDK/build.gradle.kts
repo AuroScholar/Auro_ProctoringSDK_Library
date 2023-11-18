99bf1dcfada90565369dab99e0a7b2695853a112
AuroScholar <auroscholar@gmail.com>
2023-11-18 14:00:49 +0530

    commit with 0.0.35 by manju

--- AuroProctoringSDK/build.gradle.kts
+++ AuroProctoringSDK/build.gradle.kts
@@ -95,7 +95,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.azzadpandit1"
                 artifactId = "Auro-Proctoring-SDK"
-                version = "0.0.34"
+                version = "0.0.35"
             }
         }
     }
