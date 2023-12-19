e1258b27eb973315ac2dbd8a2d97ec88d716161d
AuroScholar <71823266+AuroScholar@users.noreply.github.com>
2023-12-19 17:07:40 +0530

    Update build.gradle.kts WITH TAG 0.1.0

--- AuroProctoringSDK/build.gradle.kts
+++ AuroProctoringSDK/build.gradle.kts
@@ -108,7 +108,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.AuroScholar"
                 artifactId = "Auro_Proctoring_Library"
-                version = "0.0.9"
+                version = "0.1.0"
             }
         }
     }
