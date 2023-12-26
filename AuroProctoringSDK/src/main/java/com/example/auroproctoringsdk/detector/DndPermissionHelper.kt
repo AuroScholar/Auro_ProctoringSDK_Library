17487a5c604a8f517fbc24ed626433ce156a7819
azzadpandit1 <azzad.pandit@aurosociety.org>
2023-12-26 12:17:58 +0530

    dnd permission ask in onResume and off dnd onStop and onPause

--- AuroProctoringSDK/src/main/java/com/example/auroproctoringsdk/detector/DndPermissionHelper.kt
+++ AuroProctoringSDK/src/main/java/com/example/auroproctoringsdk/detector/DndPermissionHelper.kt
@@ -9,14 +9,16 @@ import com.example.auroproctoringsdk.dnd.DNDManagerHelper
 
 class DndPermissionHelper(private val context: Context) {
 
-     val alertDialog: AlertDialog = AlertDialog.Builder(context)
+    /* private var positiveButtonClicked = false
+
+    val alertDialog: AlertDialog = AlertDialog.Builder(context)
         .setTitle(context.getString(R.string.permission_required))
         .setMessage(R.string.to_enable_do_not_disturb_mode_you_need_to_grant_permission)
         .setPositiveButton(R.string.goto_permission) { _, _ ->
-            if (!DNDManagerHelper(context).checkDndPermission()){
+            if (!DNDManagerHelper(context).checkDndPermission()) {
                 val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
                 context.startActivity(intent)
-            }else{
+            } else {
                 hideAlertDialog()
             }
 
@@ -26,15 +28,59 @@ class DndPermissionHelper(private val context: Context) {
         .create()
 
     fun showAlertDialog() {
-        if (!DNDManagerHelper(context).checkDndPermission()){
+        if (!DNDManagerHelper(context).checkDndPermission()) {
             alertDialog.show()
+        }
+
+        if (!DNDManagerHelper(context).checkDndPermission() && !positiveButtonClicked) {
+            alertDialog.show()
+        }
+
+    }
+
+    fun hideAlertDialog() {
+        if (alertDialog.isShowing) {
+            alertDialog.hide()
+            alertDialog.dismiss()
+        }
+    }*/
+
+
+    var alertDialog: AlertDialog? = null
+    var positiveButtonClicked = false
+
+    fun showAlertDialog() {
+        if (!DNDManagerHelper(context).checkDndPermission() && !positiveButtonClicked) {
+            if (alertDialog == null) {
+                createAlertDialog()
+            }
+            alertDialog?.show()
         }
     }
 
     fun hideAlertDialog() {
-        if (alertDialog.isShowing){
-            alertDialog.hide()
-            alertDialog.dismiss()
+        if (alertDialog?.isShowing == true) {
+            alertDialog?.hide()
+            alertDialog?.dismiss()
         }
+    }
+
+    private fun createAlertDialog() {
+        alertDialog = AlertDialog.Builder(context)
+            .setTitle(context.getString(R.string.permission_required))
+            .setMessage(R.string.to_enable_do_not_disturb_mode_you_need_to_grant_permission)
+            .setPositiveButton(R.string.goto_permission) { dialog, _ ->
+                if (!positiveButtonClicked) {
+                    positiveButtonClicked = true
+                    if (!DNDManagerHelper(context).checkDndPermission()) {
+                        dialog.dismiss()
+                        val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+                        context.startActivity(intent)
+                    } else {
+                        hideAlertDialog()
+                    }
+                }
+            }
+            .create()
     }
 }
