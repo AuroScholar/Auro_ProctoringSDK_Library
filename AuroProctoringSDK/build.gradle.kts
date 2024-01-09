d7ac21092ba91e36ba825b75d14c72e5a5ebe547
azzadpandit1 <azzad.pandit@aurosociety.org>
2024-01-09 13:57:58 +0530

    [bug][fixed] A0-1593 Firebase-Proctoring-Issues time process done

--- AuroProctoringSDK/build.gradle.kts
+++ AuroProctoringSDK/build.gradle.kts
@@ -80,7 +80,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.AuroScholar"
                 artifactId = "Auro_Proctoring_Library"
-                version = "0.3.2"
+                version = "0.3.4"
             }
         }
     }
