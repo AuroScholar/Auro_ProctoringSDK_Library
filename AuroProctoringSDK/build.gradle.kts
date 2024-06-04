d3cc2f88e04b6bd9d5749cf55335ac25df804af7
AzzadPandit <azzad.pandit@aurosociety.org>
2024-06-04 15:19:16 +0530

    text add ok [fixed]

--- AuroProctoringSDK/build.gradle.kts
+++ AuroProctoringSDK/build.gradle.kts
@@ -80,7 +80,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.AuroScholar"
                 artifactId = "Auro_Proctoring_Library"
-                version = "0.4.6"
+                version = "0.4.7"
             }
         }
     }
